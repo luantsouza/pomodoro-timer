9901e5c263f907de0b1aa149034b638b8533f74f
Luan Souza <luansouzacontato@outlook.com>
2022-03-18 16:23:00 -0300

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const favicon = document.getElementById("favicon");
+const title = document.querySelector("title");
 const body = document.querySelector("body");
 const containerClock = document.querySelector(".container-clock")
 const clockCard = document.querySelector(".clock-card");
@@ -12,49 +14,84 @@ var timePomodoro = 4; //1499
 var timePomodoroRest = 4; //299
 
 startButton.addEventListener('click',() => {
-    countdownSet("on");
-    startButton.style.opacity = "0";
-    pauseButton.style.justifyContent = "center";
-    if(timePomodoro === -1){
-        timePomodoro = 4;
+    
+    if( 1 < timePomodoro <= 4 && (timePomodoro != -2)){
+        countdownSet("on");
+        startButton.style.display = "none";
+    }else if(timePomodoro < 0){
+        startButton.style.display = "none";
+        return countdownRest();
     }
-    if(timePomodoroRest === -1){
-        timePomodoroRest === 4;
-    }
+    
 })
 
 pauseButton.addEventListener('click',()=>{
     countdownSet("off");
-    startButton.style.opacity = "1";
+    startButton.style.display = "flex";
+
 })
 
 function countdownSet(state){
         
-        if(state === "on"){
-            this.time = timeInterval = setInterval(countdownMath,1000);
+    if(state === "on"){
+        this.time = timeInterval = setInterval(countdownMath,1000);
+        title.textContent = clockMinutes.textContent+clockSeconds.textContent + " Pomodoro Timer"
+
+        function countdownMath(){
+            const minutes = Math.floor((timePomodoro / 60));
+            const seconds = Math.round(timePomodoro % 60);
             
-            function countdownMath(){
-                const minutes = Math.floor((timePomodoro / 60));
-                const seconds = Math.round(timePomodoro % 60);
-            
-                clockMinutes.textContent = minutes + ":";
-                clockSeconds.textContent = seconds;
-            
-                timePomodoro = timePomodoro - 1;
+            title.textContent = minutes +":" + (seconds < 10 ? "0" + seconds : seconds) + " Pomodoro Timer";
+
+            clockMinutes.textContent = minutes + ":";
+            (seconds < 10 ? clockSeconds.textContent = "0" + seconds : clockSeconds.textContent = seconds); 
+            timePomodoro = timePomodoro - 1;
     
-                if(timePomodoro === -1){
-                    clockMinutes.textContent = "0:";
-                    clockSeconds.textContent = "5";
-                    return clockStop();
-                }
-            }
-        }else if(state === "off"){
-            if(timePomodoroRest === 4){
-                return countdownStop(timeInterval);
-            }else{
-                return countdownStopRest(timeIntervalRest);
+            if(timePomodoro === -2){
+                clockMinutes.textContent = "0:";
+                clockSeconds.textContent = "05";
+                title.textContent = "Pomodoro Timer"
+                timePomodoroRest = 4;
+                return clockStop();
             }
         }
+
+    }else if(state === "off"){
+        if(timePomodoroRest === 4){
+            return countdownStop(timeInterval);
+        }else{
+            return countdownStopRest(timeIntervalRest);
+        }
+    }
+}
+
+function countdownRest(){
+
+    this.time = timeIntervalRest = setInterval(countdownMathRest,1000);
+    
+    favicon.setAttribute("href", "/images/favicon2-32x32.png");
+    title.textContent = clockMinutes.textContent+clockSeconds.textContent + " Pomodoro Timer"    
+
+    function countdownMathRest(){
+        const minutes = Math.floor((timePomodoroRest / 60));
+        const seconds = Math.round(timePomodoroRest % 60);
+            
+        title.textContent = minutes +":" + (seconds < 10 ? "0" + seconds : seconds) + " Pomodoro Timer" ;
+
+        clockMinutes.textContent = minutes + ":";
+        (seconds < 10 ? clockSeconds.textContent = "0" + seconds : clockSeconds.textContent = seconds); 
+            
+        timePomodoroRest = timePomodoroRest - 1;
+
+        if(timePomodoroRest === -2){
+        clockMinutes.textContent = "0:";
+        clockSeconds.textContent = "05";
+        title.textContent = "Pomodoro Timer";
+        favicon.setAttribute("href", "/images/favicon-32x32.png");
+        timePomodoro = 4;
+        return clockStopRest();
+    }
+    }
 }
 
 function countdownStop(time){
@@ -69,42 +106,30 @@ function countdownStopRest(time){
 
 function clockStop(){
     clearInterval(timeInterval);
-    body.style.backgroundColor = "hsl(159, 53%, 28%)";
-    containerClock.style.backgroundColor = "hsl(159,53%,34%)";
-    clockCard.style.backgroundColor = "hsl(159,53%,39%)";
-
+    changeGreen();
     return countdownRest();
 }
 
 function clockStopRest(){
     clearInterval(timeIntervalRest);
-    startButton.style.opacity = "1";
+    startButton.style.display = "flex";
     startButton.textContent = "START";
+    changeRed();
+}
+
+function changeGreen(){
+    startButton.style.color = "hsl(159,53%,34%)";
+    pauseButton.style.color = "hsl(159,53%,34%)";
+    body.style.backgroundColor = "hsl(159, 53%, 28%)";
+    containerClock.style.backgroundColor = "hsl(159,53%,34%)";
+    clockCard.style.backgroundColor = "hsl(159,53%,39%)";
+}
+
+function changeRed(){
+    startButton.style.color = "hsl(6, 57%, 49%)";
+    pauseButton.style.color = "hsl(6, 57%, 49%)";
     body.style.backgroundColor = "hsl(6, 57%, 49%)";
     containerClock.style.backgroundColor = "hsl(6, 57%, 55%)";
     clockCard.style.backgroundColor = "hsl(6, 57%, 60%)";   
 }
 
-function countdownRest(){
-    if(timePomodoroRest != 4){
-        timePomodoroRest === 4;
-    }
-
-    this.time = timeIntervalRest = setInterval(countdownMathRest,1000);
-            
-        function countdownMathRest(){
-            const minutes = Math.floor((timePomodoroRest / 60));
-            const seconds = Math.round(timePomodoroRest % 60);
-            
-            clockMinutes.textContent = minutes + ":";
-            clockSeconds.textContent = seconds;
-            
-            timePomodoroRest = timePomodoroRest - 1;
-
-            if(timePomodoroRest === -1){
-                clockMinutes.textContent = "0:";
-                clockSeconds.textContent = "5";
-                return clockStopRest();
-            }
-        }
-}
